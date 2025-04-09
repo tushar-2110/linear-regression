38ae7350bd415b4d5299cf2a1f9f75d9482229ed
Tusharshourlywork <tusharsharma.interviews@gmail.com>
2025-04-09 21:47:40 +0530

    minor bug bix

--- main.py
+++ main.py
@@ -3,24 +3,21 @@ from pydantic import BaseModel
 import joblib
 import pandas as pd
 
-
 model = joblib.load("housepricemodel.pk")
 app = FastAPI()
-
 
 class HouseFeatures(BaseModel):
     GrLivArea: float
     OverallQual: int
 
-    @app.get("/")
-    def root():
-        return {"message": "House Price Prediction API is up"}
+@app.get("/")
+def root():
+    return {"message": "House Price Prediction API is up"}
 
-    @app.post("/predict")
-    def predict_price(features: HouseFeatures):
-        input_df = pd.DataFrame(
-            [{"GrLivArea": features.GrLivArea, "OverallQual": features.OverallQual}]
-        )
-
+@app.post("/predict")
+def predict_price(features: HouseFeatures):
+    input_df = pd.DataFrame(
+        [{"GrLivArea": features.GrLivArea, "OverallQual": features.OverallQual}]
+    )
     prediction = model.predict(input_df)[0]
     return {"predicted price is ": round(prediction, 2)}
